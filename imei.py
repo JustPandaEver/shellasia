ee91b12b319d540fd89ae85d3187f1a7bd34f41b
PandaEver <63947823+JustPandaEver@users.noreply.github.com>
2021-12-26 18:43:51 +0700

    Update imei.py

--- imei.py
+++ imei.py
@@ -1,3 +1,3 @@
 import subprocess
-imei = (subprocess.check_output(['getprop','ro.ril.oem.imei'])[:-1])
+imei = (subprocess.check_output(['getprop','persist.radio.imei'])[:-1])
 print(imei.decode("utf-8"))
